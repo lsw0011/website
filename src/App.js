a874d0b04759df113ae977e627fcc5729eed9889
luke westerfield <lukeswesterfield@gmail.com>
2019-03-02 09:43:57 -0500

    make it better

--- src/App.js
+++ src/App.js
@@ -6,21 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          <div className="front-page">
+            <div>
+              <p className="front-text">HELLO MY NAME IS LUKE</p>
+            </div>
+          </div>
+        </div>
     );
   }
 }
