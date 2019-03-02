c15466e8e7e8fc86e0ee1474f695a5bd8122882b
luke westerfield <lukeswesterfield@gmail.com>
2019-03-02 09:54:12 -0500

    make it better

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import FixedHeader from './FunctionalComponents/FixedHeader'
+import FixedHeader from './FunctionalComponents/FixedHeader/FixedHeader'
 import Hoc from './hoc';
 import './App.css';
 
