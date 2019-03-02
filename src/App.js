b3436f02d7b1127324b8a117109a8f32d84b4392
luke westerfield <lukeswesterfield@gmail.com>
2019-03-02 10:00:14 -0500

    make it better

--- src/App.js
+++ src/App.js
@@ -5,6 +5,32 @@ import Hoc from './hoc';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    document.addEventListener('scroll', (evt) => {
+
+      const fixedHeaderContainer = document.querySelector('.fixed-header-container');
+      const fixedHeader = document.querySelector('.fixed-header') || document.querySelector('.fixed-header-alt');
+      const fixedHeaderItem = document.querySelectorAll('.fixed-header-item');
+      const fixedHeaderItemAlt = document.querySelectorAll('.fixed-header-item-alt');
+
+      if(document.documentElement.scrollTop >= 200){
+        fixedHeaderContainer.style.backgroundColor = "white"
+        fixedHeader.classList.add('fixed-header-alt');
+        fixedHeader.classList.remove('fixed-header');
+        fixedHeaderItem.forEach(element => {
+          element.className = 'fixed-header-item-alt';
+        })
+      }else{
+        fixedHeaderContainer.style.backgroundColor = 'transparent';
+        fixedHeader.classList.add('fixed-header');
+        fixedHeader.classList.remove('fixed-header-alt');
+        fixedHeaderItemAlt.forEach(element => {
+          element.className = 'fixed-header-item';
+        })
+      }
+
+    })
+  }
   render() {
     return (
       <Hoc>
