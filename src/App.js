1e07bc97ca4a57d1cf9bb6f66fd8fd32df23b5d7
luke westerfield <lukeswesterfield@gmail.com>
2019-05-14 10:50:24 -0400

    PORT

--- src/App.js
+++ src/App.js
@@ -18,12 +18,11 @@ class App extends Component {
 
   componentDidMount() {
     document.addEventListener('scroll', (evt) => {
-
       const fixedHeaderContainer = document.querySelector('.fixed-header-container');
       const fixedHeader = document.querySelector('.fixed-header') || document.querySelector('.fixed-header-alt');
       const fixedHeaderItem = document.querySelectorAll('.fixed-header-item');
       const fixedHeaderItemAlt = document.querySelectorAll('.fixed-header-item-alt');
-
+      console.log(document.documentElement.scrollTop)
       if(document.documentElement.scrollTop >= 200){
         fixedHeaderContainer.style.backgroundColor = "white"
         fixedHeader.classList.add('fixed-header-alt');
@@ -41,6 +40,21 @@ class App extends Component {
       }
 
     })
+  }
+
+  scrollDoc = (targetPosition, currentPosition) => {
+    let i = 1;
+    const step = Math.floor((targetPosition - currentPosition) / 30);
+    console.log(step)
+    const scrollInterval = setInterval(() => {
+      console.log(i)
+      document.documentElement.scrollTop += step; 
+      i++; 
+      if(i === 30){
+        document.documentElement.scrollTop = targetPosition
+        clearInterval(scrollInterval)
+      }
+    }, 10)
   }
 
   delayStyle = (add) => {
@@ -76,23 +90,24 @@ class App extends Component {
     let bars = this.loadAnimation();
     return (
       <Hoc>
-        <FixedHeader />
-        <div className="App">
-          <div className="front-page">
-            <div>
-              <p className="front-text">HELLO MY NAME IS LUKE</p>
-            </div>
+        <FixedHeader scrollDoc = {this.scrollDoc} />
+        <div className="front-page">
+          <div>
+            <p className="front-text">HELLO MY NAME IS LUKE</p>
           </div>
         </div>
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 139 129" xmlnsXlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 129 139" className="down-arrow">
+        <div className="down-arrow-container" onClick={() => this.scrollDoc(575, document.documentElement.scrollTop)}>
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 139 129" xmlnsXlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 129 139" className="down-arrow">
             <g fill="#666666">
               <path d="m121.3,34.6c-1.6-1.6-4.2-1.6-5.8,0l-51,51.1-51.1-51.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l53.9,53.9c0.8,0.8 1.8,1.2 2.9,1.2 1,0 2.1-0.4 2.9-1.2l53.9-53.9c1.7-1.6 1.7-4.2 0.1-5.8z"/>
             </g>
           </svg>
-        <Summary />
-        <Skills />
-        <Projects />
-         
+        </div>
+        <div className="website-content">
+          <Summary />
+          <Skills />
+          <Projects />
+        </div>
       </Hoc>
     )
   }
--- src/FunctionalComponents/FixedHeader/FixedHeader.js
+++ src/FunctionalComponents/FixedHeader/FixedHeader.js
@@ -7,18 +7,19 @@ import './FixedHeader.css';
 const fixedHeader = (props) => (    
     <div className="fixed-header-container">
         <div className="fixed-header header-transition background-transition">
-        <div className="fixed-header-item fixed-header-item-gradient">
-            HOME
-        </div>
-        <div className="fixed-header-item fixed-header-item-gradient">
-            PROJECTS
-        </div>
-        <div className="fixed-header-item fixed-header-item-gradient">
-            EDUCATION
-        </div>
-        <div className="fixed-header-item fixed-header-item-gradient">
-            INTERESTS
-        </div>
+            <div className="fixed-header-item fixed-header-item-gradient" onClick={() => props.scrollDoc(0, document.documentElement.scrollTop)}>
+                HOME
+            </div>
+            <div className="fixed-header-item fixed-header-item-gradient" onClick={() => props.scrollDoc(575, document.documentElement.scrollTop)}>
+                SUMMARY
+            </div>
+            <div className="fixed-header-item fixed-header-item-gradient" onClick={() => props.scrollDoc(1922, document.documentElement.scrollTop)}>
+                PROJECTS
+            </div>
+            <div className="fixed-header-item fixed-header-item-gradient" onClick={() => props.scrollDoc(1350, document.documentElement.scrollTop)}>
+                SKILLS
+            </div>
+           
         </div>
     </div>
 )
